e5bb2359a24745cac161ad5dc36c6cc6d224d94c
Fellipe Calleia <fellipe@calleia.com.br>
2018-12-18 01:53:36 -0200

    Add parameter to configure the output filename

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <getopt.h>
 
 int main(int argc, char** argv) {
+
+	static struct option long_options[] = {
+		{"output", required_argument, NULL, 'o'},
+		{NULL, 0, NULL, 0}
+	};
+
+	int option;
+	char* output_filename = "output.file";
+
+	while ((option = getopt_long(argc, argv, "o:", long_options, NULL)) != -1) {
+		switch (option) {
+			case 'o':
+				output_filename = optarg;
+				break;
+			default:
+				fprintf(stderr, "Invalid parameter.\n");
+				// TODO: call show_help()
+		}
+	}
+
 	return 0;
 }
