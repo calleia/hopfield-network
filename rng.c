7ac20b1ae5423e04b7903c4bed5e65fad484d3a2
Fellipe Calleia <fellipe@calleia.com.br>
2019-04-16 23:58:34 -0300

    Fix roll function

--- rng.c
+++ rng.c
@@ -1,8 +1,15 @@
 #include <time.h>
 #include <stdlib.h>
 
+int flag = 0;
+
+// Generates a random int between (0) and (max - 1)
 int roll(int max) {
-	srand(time(NULL));
+	if (flag == 0) {
+		srand(time(NULL));
+		flag++;
+	}
+	
 	int random = rand();
 
 	return random % max;
