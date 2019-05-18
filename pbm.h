79e92554004032e6af42be4e15cdb870767e05ff
Fellipe Calleia <fellipe@calleia.com.br>
2019-05-18 14:30:19 -0300

    Remove loadPBM() function

--- pbm.h
+++ pbm.h
@@ -1,13 +1,6 @@
 #ifndef PBM_H_
 #define PBM_H_
 
-typedef struct PBMImage { 
-    unsigned long width, height;
-    char* data;
-} PBMImage;
-
-PBMImage* loadPBM(FILE* pFile);
-
 char* load_pbm_image(char* filename);
 
 void save_pbm_image(char* filename, char* pixels);
