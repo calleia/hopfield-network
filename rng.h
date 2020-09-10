0ff0a044a11c0861bbf49d348f4c7df4afd1e6a4
Fellipe Calleia <fellipe@calleia.com.br>
2020-09-09 21:28:23 -0300

    Fix rng.h indentation

--- rng.h
+++ rng.h
@@ -4,11 +4,11 @@
 /*
 ****************************************************************************
 
-            Random Number Generator
-              Version 1.0,  Jan. 13, 1992
-              From Numerical Recipies in C
+			Random Number Generator
+			Version 1.0, Jan. 13, 1992
+			From Numerical Recipies in C
 
-        Generates random number in interval [0, 1].
+		Generates random number in interval [0, 1].
 
 ****************************************************************************
 */
