16d86c20addda0b62ff63da6a5361c52143013ee
Fellipe Calleia <fellipe@calleia.com.br>
2020-09-09 21:23:56 -0300

    Fix rng.c indentation

--- rng.c
+++ rng.c
@@ -3,11 +3,11 @@
 /*
 ****************************************************************************
 
-            Random Number Generator
-              Version 1.0,  Jan. 13, 1992
-              From Numerical Recipies in C
+			Random Number Generator
+			Version 1.0, Jan. 13, 1992
+			From Numerical Recipies in C
 
-        Generates random number in interval [0, 1].
+		Generates random number in interval [0, 1].
 
 ****************************************************************************
 */
@@ -19,38 +19,38 @@
 
 float ran3(long *idum)
 {
-    static int inext, inextp;
-    static long ma[56];
-    static int iff = 0;
-    long mj, mk;
-    int i, ii, k;
+	static int inext, inextp;
+	static long ma[56];
+	static int iff = 0;
+	long mj, mk;
+	int i, ii, k;
 
-    if (*idum < 0 || iff == 0) {
-        iff = 1;
-        mj = labs(MSEED - labs(*idum));
-        mj %= MBIG;
-        ma[55] = mj;
-        mk = 1;
-        for (i=1;i<=54;i++) {
-            ii = (21 * i) % 55;
-            ma[ii] = mk;
-            mk = mj - mk;
-            if (mk < MZ) mk += MBIG;
-            mj = ma[ii];
-        }
-        for (k=1;k<=4;k++)
-            for (i=1;i<=55;i++) {
-                ma[i] -= ma[1 + (i + 30) % 55];
-                if (ma[i] < MZ) ma[i] += MBIG;
-            }
-        inext = 0;
-        inextp = 31;
-        *idum = 1;
-    }
-    if (++inext == 56) inext = 1;
-    if (++inextp == 56) inextp = 1;
-    mj = ma[inext] - ma[inextp];
-    if (mj < MZ) mj += MBIG;
-    ma[inext] = mj;
-    return mj * FAC;
+	if (*idum < 0 || iff == 0) {
+		iff = 1;
+		mj = labs(MSEED - labs(*idum));
+		mj %= MBIG;
+		ma[55] = mj;
+		mk = 1;
+		for (i=1;i<=54;i++) {
+			ii = (21 * i) % 55;
+			ma[ii] = mk;
+			mk = mj - mk;
+			if (mk < MZ) mk += MBIG;
+			mj = ma[ii];
+		}
+		for (k=1;k<=4;k++)
+			for (i=1;i<=55;i++) {
+				ma[i] -= ma[1 + (i + 30) % 55];
+				if (ma[i] < MZ) ma[i] += MBIG;
+			}
+		inext = 0;
+		inextp = 31;
+		*idum = 1;
+	}
+	if (++inext == 56) inext = 1;
+	if (++inextp == 56) inextp = 1;
+	mj = ma[inext] - ma[inextp];
+	if (mj < MZ) mj += MBIG;
+	ma[inext] = mj;
+	return mj * FAC;
 }
